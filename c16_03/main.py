06115636f2afb47b5aa3b9cadc6f1476a6828ca1
AndreaM99 <andrea.masse@etu.u-paris.fr>
2021-03-19 15:35:30 +0000

    ajout question 4 a) et b)

--- c16_03/main.py
+++ c16_03/main.py
@@ -11,70 +11,13 @@ note8 = ('eleve2', 'math', 14)
 
 notes = [note1, note2, note3, note4, note5, note6,note7,note8]
 
-# Question 4 a)
-from statistics import mean
-x = (13,13,15,12,13,12)
-print("Moyenne est ",mean(x))
-
+print("-- Question 4 a) --")
 notes_elv1 = [note for note in notes if note[0] == 'eleve1']
+print(notes_elv1)
+print(sum(note[2] for note in notes_elv1)/len(notes_elv1))
 
-# Question 4 b)
-y = (13,13,12)
-print("Moyenne en maths est ",round(mean(y),2))
+print("-- Question 4 b) --")
+notes_elv1_maths = [n for n in notes_elv1 if n[1] == 'math']
+print(notes_elv1_maths)
+print(sum(n[2] for n in notes_elv1_maths)/len(notes_elv1_maths))
 
-# Question 4 c)
-def moyenne_tuplestest(notes,eleve,matiere):
-  s = 0
-  i = 0
-  for x in notes:
-    if x[0] == eleve and x[1]== matiere:
-      s = s + x[2]
-      i = i + 1
-      print(i)
-  moy = s / i
-  return round(moy,2)
-
-print(moyenne_tuplestest(notes,'eleve1','math'))
-
-def moyenne_tuples (liste_note,eleve = None,matiere = None):
-  if matiere != None :
-    notes = [ liste_note[i][2] for i in range(len(liste_note)) if liste_note[i][0] == eleve and liste_note[i][1] == matiere]
-    moyenne = sum(notes)/len(notes)
-    return moyenne
-  else :
-    notes = [ liste_note[i][2] for i in range(len(liste_note)) if liste_note[i][0] == eleve ]
-    moyenne = sum(notes)/len(notes)
-    return moyenne
- 
-#test
-print("le résultat de la fonction : " , moyenne_tuples(notes,"eleve2","math"))
-
-
-print("")
-##########
-class Note:
-  def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
-    self.eleve = eleve # eleve defini dans le init
-    self.matiere = matiere
-    self.valeur = valeur
-
-
-  def afficher(self):
-    print('eleve :', self.eleve, 'matiere :', self.matiere, 'note :', self.valeur)
-
-
-onote = Note('eleve1', 'maths', 13)
-print(onote.eleve)
-print(onote.matiere)
-print(onote.valeur)
-Note.afficher(onote)
-
-print("")
-##########
-
-# Question 5)
-eleve = [notes[i][0] for i in range(len(notes))]
-print(eleve)
-
-onotes = Note("eleve1", 'maths',13)
-Note.afficher(onotes)
