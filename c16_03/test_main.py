e2695ac98b7e9947a54f8e9247b857477d474ee6
AndreaM99 <andrea.masse@etu.u-paris.fr>
2021-03-19 18:24:56 +0000

    ajout des tests question 11 (problemes au niveau du test_main.py, du supprimer le .main car marche pas)

--- c16_03/test_main.py
+++ c16_03/test_main.py
@@ -1,10 +1,15 @@
-from .main import Note
+from main import Note
 
 note = Note('xx','yy','zzz')
 print(note)
 
 def test_ajout():
   Note.vider()
-  # assert
-  Note(eleve = 'eleve1', matiere='math', valeur = 12)
-  # assert
+  assert len(Note.instances) == 0
+  Note(eleve = 'eleve1', matiere='math', valeur = 12) # quand on commence des mots clés, on les continu !! + Note pas Node.
+  assert len(Note.instances) == 0
+
+
+# pip install pytest-cov
+# pytest --cov .abs
+# toute la couverture de code car tout le fichier est parcouru.
